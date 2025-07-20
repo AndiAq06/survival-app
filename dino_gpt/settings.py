14640060b13213183f1c270284369596a2516a3c
ANDI ARDIANSYAH NASIR <222111890@stis.ac.id>
2025-07-20 09:10:16 +0700

    final code v1

--- dino_gpt/settings.py
+++ dino_gpt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['survival-app-main-e8eccdc.kuberns.cloud', 'localhost', '127.0.0.1']
 
 # Load OpenAI API Key
 OPENAI_API_KEY = config('OPENAI_API_KEY')
